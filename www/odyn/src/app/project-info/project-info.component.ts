0dbcfa2789b5594cb3b3aaac8b19a9f83204eac8
Mateusz Szymczak <84203463+mszymczak710@users.noreply.github.com>
2023-01-16 22:30:36 +0100

    Delete www/odyn directory

--- www/odyn/src/app/project-info/project-info.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-project-info',
-  templateUrl: './project-info.component.html',
-  styleUrls: ['./project-info.component.scss'],
-})
-export class ProjectInfoComponent {}
--- www/odyn/src/app/work-schedule/models/work-schedule.model.ts
+++
@@ -1,6 +0,0 @@
-export interface WorkSchedule {
-  year: number;
-  month: string;
-  day: string;
-  detail: string;
-}
