2f463dacf0fff8ab33ed6a8be9137149a65572a3
XewoniX <xewonix36@gmail.com>
2023-02-28 16:25:40 +0100

    Widok z kamery
    Dodanie widoku z kamery przy uzyciu biblioteki cameraX, oraz przygotowanie do zapisu zdjec oraz video

--- app/src/main/java/com/example/odyn/MainScreen.java
+++ app/src/main/java/com/example/odyn/MainScreen.java
@@ -1,18 +1,82 @@
 package com.example.odyn;
 
+import android.Manifest;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.ImageCapture;
+import androidx.camera.core.ImageCaptureException;
+import androidx.camera.core.Preview;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.lifecycle.LifecycleOwner;
 
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.io.File;
+import java.util.concurrent.ExecutionException;
+
 // ekran główny aplikacji
 public class MainScreen extends AppCompatActivity {
 
+    public PreviewView previewView;
+    private ImageCapture imageCapture;
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private static final int MY_WRITE_EXTERNAL_STORAGE = 100;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_screen);
+
+
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        }
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_WRITE_EXTERNAL_STORAGE);
+        }
+
+        previewView = findViewById(R.id.previewView);
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindPreview(ProcessCameraProvider cameraProvider) {
+
+        // Set up the preview use case
+        Preview preview = new Preview.Builder()
+                .build();
+
+
+        CameraSelector cameraSelector = new CameraSelector.Builder()
+                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
+                .build();
+
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+
+        ImageCapture.Builder builder = new ImageCapture.Builder();
+        imageCapture = builder
+                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
+                .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation())
+                .build();
+        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview, imageCapture);
     }
     // Otwórz panel menu
     public void onClickMenu(View view) {
@@ -20,7 +84,22 @@ public class MainScreen extends AppCompatActivity {
     }
     // Zrób zdjęcie
     public void onClickPhoto(View view) {
-        //
+        // Create a file to store the image
+        File file = new File(getExternalMediaDirs()[0], "image.jpg");
+
+        // Set up the output file and capture the image
+        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(file).build();
+        imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(this), new ImageCapture.OnImageSavedCallback() {
+            @Override
+            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
+                // The image has been saved to the file
+            }
+
+            @Override
+            public void onError(@NonNull ImageCaptureException exception) {
+                // Handle any errors here
+            }
+        });
     }
     // Nagrywanie awaryjne
     public void onClickEmergency(View view) {
