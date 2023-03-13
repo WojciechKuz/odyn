38d76d05a2bda2eb6b2afded8bbf69633eb8f8b8
WojciechKuz <104651306+WojciechKuz@users.noreply.github.com>
2023-03-13 22:11:20 +0100

    usunięcie zakomentowanego kodu

--- app/src/main/java/com/example/odyn/MainScreen.java
+++ app/src/main/java/com/example/odyn/MainScreen.java
@@ -36,63 +36,7 @@ public class MainScreen extends AppCompatActivity {
         // tu znajdują się rzeczy związane z inicjalizacją kamery
         camAccess = new CamAccess(this, previewView);
     }
-/*  WYCIĘTY KOD. wróć tu jeśli coś niedziała.
 
-    POLE TEJ KLASY:
-    private ImageCapture imageCapture;
-
-    FRAGMENT ONCREATE():
-    // to poniżej przeniesione do CamAccess. usunąć.
-        ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(this);
-        cameraProviderFuture.addListener(() -> {
-            try {
-                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
-                bindPreview(cameraProvider);
-            } catch (ExecutionException | InterruptedException e) {
-
-            }
-        }, ContextCompat.getMainExecutor(this));
-    KONIEC
-
-    METODA BINDPREVIEW():
-    private void bindPreview(ProcessCameraProvider cameraProvider) { // przeniesione do CamAccess. usunąć.
-
-        // Set up the preview use case
-        Preview preview = new Preview.Builder()
-                .build();
-
-
-        CameraSelector cameraSelector = new CameraSelector.Builder()
-                .requireLensFacing(CameraSelector.LENS_FACING_BACK)
-                .build();
-
-
-        preview.setSurfaceProvider(previewView.getSurfaceProvider());
-
-
-        ImageCapture.Builder builder = new ImageCapture.Builder();
-        imageCapture = builder
-                .setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation())
-                .build();
-        Camera camera = cameraProvider.bindToLifecycle((LifecycleOwner)this, cameraSelector, preview, imageCapture);
-    }
-
-    FRAGMENT ONCLICKPHOTO() ZA UTWORZENIEM PLIKU:
-    // Set up the output file and capture the image
-        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(file).build();
-        imageCapture.takePicture(outputFileOptions, ContextCompat.getMainExecutor(this), new ImageCapture.OnImageSavedCallback() {
-            @Override
-            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
-                // The image has been saved to the file
-            }
-
-            @Override
-            public void onError(@NonNull ImageCaptureException exception) {
-                // Handle any errors here
-            }
-        });
- */
 
     // Otwórz panel menu
     public void onClickMenu(View view) {
