aa93c0d80b6a2b613477f7a06cf0612e90be35f2
WojciechKuz <104651306+WojciechKuz@users.noreply.github.com>
2023-03-13 21:41:06 +0100

    przeniesienie do CamAccess2

--- app/src/main/java/com/example/odyn/MainScreen.java
+++ app/src/main/java/com/example/odyn/MainScreen.java
@@ -1,28 +1,15 @@
 package com.example.odyn;
 
 import android.Manifest;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.Camera;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureException;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
-import androidx.lifecycle.LifecycleOwner;
 
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
 
-import com.google.common.util.concurrent.ListenableFuture;
-
 import java.io.File;
-import java.util.concurrent.ExecutionException;
 
 // ekran główny aplikacji
 public class MainScreen extends AppCompatActivity {
