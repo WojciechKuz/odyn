46581d16380f54d9d92a1df15dd92ec76daa2b8c
XewoniX <xewonix36@gmail.com>
2023-03-21 19:06:17 +0100

    kamera_testy

--- app/src/main/java/com/example/odyn/CamAccess.java
+++ app/src/main/java/com/example/odyn/CamAccess.java
@@ -1,15 +1,29 @@
 package com.example.odyn;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import androidx.camera.core.Camera;
 import androidx.camera.core.CameraSelector;
 import androidx.camera.core.ImageCapture;
 import androidx.camera.core.ImageCaptureException;
 import androidx.camera.core.Preview;
+import androidx.camera.core.VideoCapture;
 import androidx.camera.lifecycle.ProcessCameraProvider;
 import androidx.camera.view.PreviewView;
+import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.lifecycle.LifecycleOwner;
 
@@ -17,9 +31,11 @@ import com.google.common.util.concurrent.ListenableFuture;
 
 import java.io.File;
 import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executor;
 
-public class CamAccess {
+public class CamAccess extends AppCompatActivity {
     private ImageCapture imageCapture;
+
     private Activity main; // póki co spełnia dwie role: wątek (Context) i aktywność (wyświetlanie), później warto rozważyć rozdzielenie
 
     // konstruktor. PreviewView służy do wyświetlenia w nim obrazu z kamery
@@ -29,6 +45,7 @@ public class CamAccess {
     }
 
     // te dwie poniższe funkcje służą do przygotowania kamery do przekazywania obrazu do <PreviewView> i robienia zdjęć
+    @SuppressLint("RestrictedApi")
     private void cameraProviderSetup(PreviewView prView) {
         ListenableFuture<ProcessCameraProvider> cameraProviderFuture = ProcessCameraProvider.getInstance(main);
         cameraProviderFuture.addListener(() -> {
@@ -39,6 +56,7 @@ public class CamAccess {
                 // gdzie przechwycenie ???
             }
         }, ContextCompat.getMainExecutor(main));
+
     }
     private void bindPreview(ProcessCameraProvider cameraProvider, PreviewView prView) {
 
@@ -77,5 +95,52 @@ public class CamAccess {
         });
     }
 
+    boolean isRecording = false;
+    public VideoCapture videoCapture;
+    private Executor getExecutor() {
+        return ContextCompat.getMainExecutor(this);
+    }
+    @SuppressLint("RestrictedApi")
+    public void recordVideo(File file) {
+
+                    if (isRecording == false) {
+                        isRecording = true;
+                        if (videoCapture != null) {
+                            long timeStamp = System.currentTimeMillis();
+                            ContentValues contentValues = new ContentValues();
+                            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, timeStamp);
+                            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "video/mp4");
+
+                            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
+
+                                return;
+                            }
+                            videoCapture.startRecording(
+                                    new VideoCapture.OutputFileOptions.Builder(
+                                            getContentResolver(),
+                                            MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                                            contentValues
+                                    ).build(),
+                                    getExecutor(),
+                                    new VideoCapture.OnVideoSavedCallback() {
+                                        @Override
+                                        public void onVideoSaved(@NonNull VideoCapture.OutputFileResults outputFileResults) {
+                                            Toast.makeText(CamAccess.this, "Zapisywanie...", Toast.LENGTH_SHORT).show();
+                                        }
+                                        @Override
+                                        public void onError(int videoCaptureError, @NonNull String message, @Nullable Throwable cause) {
+                                            Toast.makeText(CamAccess.this, "Blad... : " + message, Toast.LENGTH_SHORT).show();
+                                        }
+                                    }
+                            );
+                        }
+                    } else {
+                        isRecording = false;
+                        if (videoCapture != null) {
+                            videoCapture.stopRecording();
+                        }
+            }
+
+    }
 
 }
--- app/src/main/java/com/example/odyn/MainScreen.java
+++ app/src/main/java/com/example/odyn/MainScreen.java
@@ -8,6 +8,7 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 import android.content.Intent;
+import android.widget.ImageButton;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
@@ -64,8 +65,10 @@ public class MainScreen extends AppCompatActivity {
 
     // Nagraj wideo
     public void onClickRecord(View view) {
-        //
+        File file = new FileHandler(this).createPicture();
+            camAccess.recordVideo(file);
     }
+
 
     // Z menu:
 
