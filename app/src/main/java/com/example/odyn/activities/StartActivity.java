7b1b1624a03a6747e72693e177077d254b6d961f
WojciechKuz <104651306+WojciechKuz@users.noreply.github.com>
2023-05-20 23:01:43 +0200

    Poprawienie obsługi uprawnień

--- app/src/main/java/com/example/odyn/activities/StartActivity.java
+++ app/src/main/java/com/example/odyn/activities/StartActivity.java
@@ -1,6 +1,9 @@
 package com.example.odyn.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
 import com.example.odyn.R;
 import com.example.odyn.main_service.MainService;
 
@@ -10,18 +13,32 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.util.Log;
 
+/* Do podręcznika użytkownika:
+	Należy zgodzić się na wszystkie uprawnienia.
+	Jeżeli nie chce się, aby aplikacja nagrywała dźwięk,
+	albo pokazywała lokalizację, można to wyłączyć poprzez ustawienia aplikacji.
+*/
 public class StartActivity extends AppCompatActivity {
 
-	private static final int MY_CAMERA_REQUEST_CODE = 100;
-	private static final int MY_WRITE_EXTERNAL_STORAGE = 100;
+	//private static final int MY_CAMERA_REQUEST_CODE = 100;
+	//private static final int MY_WRITE_EXTERNAL_STORAGE = 100;
+	//private static final int MY_MICROPHONE_REQUEST = 100;
 
-	private static final int MY_MICROPHONE_REQUEST = 100;
+	//private boolean cameraPermission = false;
+	//private boolean writeExternalStoragePermission = false;
+	//private boolean recordAudioPermission = false;
 
-	private boolean cameraPermission = false;
+	private static final int permCode = 100;
 
-	private boolean writeExternalStoragePermission = false;
-
-	private boolean recordAudioPermission = false;
+	// z tego zawsze potrzebne są na pewno CAMERA, WRITE_EXTERNAL_STORAGE.
+	// od ustawień zależy RECORD_AUDIO, (ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION)
+	private final String[] perms = {
+			Manifest.permission.CAMERA,
+			Manifest.permission.RECORD_AUDIO,
+			Manifest.permission.WRITE_EXTERNAL_STORAGE,
+			Manifest.permission.ACCESS_FINE_LOCATION,
+			Manifest.permission.ACCESS_COARSE_LOCATION
+	};
 
 
 	@Override
@@ -29,43 +46,61 @@ public class StartActivity extends AppCompatActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_start);
 
-		// pytania o uprawnienia
-		permissionCheck();
+		// laterTODO sprawdź, które uprawnienia są potrzebne w ustawieniach
 
-		// TODO nie uruchamiaj bez uprawnień, przycisk do ponownego zapytania o uprawn.
-		// np. w pętli co 1s sprawdzać uprawnienia, jeśli są uruchom MainService
+		// sprawdza uprawnienia, jeśli są, uruchamia MainService (start apki)
+		permCheckNew();
 
-		// start MainService
-		if(cameraPermission && writeExternalStoragePermission && recordAudioPermission) {
+	}
+
+	// sprawdzenie odpowiedzi z okna proszącego o uprawnienia
+	@Override
+	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+		StringBuilder upr = new StringBuilder("upr: ");
+		if(requestCode == permCode) {
+			boolean permissionSum = true;
+			for(int granty : grantResults) {
+				permissionSum = permissionSum && (granty == PackageManager.PERMISSION_GRANTED);
+				upr.append(granty == PackageManager.PERMISSION_GRANTED).append(", ");
+			}
+			if(permissionSum) {
+				Log.d("StartActivity", ">>> Mam uprawnienia, uruchamiam MainService");
+				startMainService();
+			}
+			else {
+				upr.append('.');
+				Log.w("StartActivity", ">>> Nie wyrażono zgody na wszystkie potrzebne uprawnienia" + upr);
+			}
+		}
+
+	}
+
+
+	// wstępne sprawdzenie
+	private void permCheckNew() {
+		if (allPerm()) {
 			startMainService();
-		}  else {
-			Log.w("StartActivity", ">>> nie uruchomiono mainService, ponieważ nie wyrażono zgody na uprawnienia");
+		}
+		else {
+			ActivityCompat.requestPermissions(this, perms, permCode);
 		}
 	}
-	private void permissionCheck() {
-		// sprawdzenie uprawnień do aparatu i pamięci wewn.
-		if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-			requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
-		} else {
-			cameraPermission = true;
-		}
-		if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-			requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_WRITE_EXTERNAL_STORAGE);
-		} else {
-			writeExternalStoragePermission = true;
-		}
-		if (checkSelfPermission(Manifest.permission.RECORD_AUDIO) != PackageManager.PERMISSION_GRANTED) {
-			requestPermissions(new String[]{Manifest.permission.RECORD_AUDIO}, MY_MICROPHONE_REQUEST);
-		} else {
-			recordAudioPermission = true;
-		}
+	private boolean allPerm() {
+		boolean permSum = true;
+		for(String perm : perms)
+			permSum = permSum && ContextCompat.checkSelfPermission(this, perm) == PackageManager.PERMISSION_GRANTED;
+		return permSum;
 	}
 
+	// uruchom aplikację
 	private void startMainService() {
 		Intent service = new Intent(this, MainService.class);
 		service.putExtra("start", 1);
 		startService(service);
 	}
+
+	@Deprecated
 	private void startMainScreen() {
 		Intent activity = new Intent(this, MainScreen.class);
 		//activity.putExtra("start", 1);
