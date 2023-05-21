e24e862ae6496032c8365e54eae302ae46edf7c5
Mateusz Szymczak <mszymczak710@o2.pl>
2023-05-21 14:06:46 +0200

    add authors and info about license in xml files

--- app/src/main/java/com/example/odyn/FileHandler.java
+++ app/src/main/java/com/example/odyn/FileHandler.java
@@ -1,6 +1,6 @@
 /*
     BSD 3-Clause License
-    Copyright (c) Wojciech Kuźbiński <wojkuzb@mat.umk.pl>, 2023
+    Copyright (c) Wojciech Kuźbiński <wojkuzb@mat.umk.pl>, Damian Gałkowski <galdam@mat.umk.pl>, 2023
 
     See https://aleks-2.mat.umk.pl/pz2022/zesp10/#/project-info for see license text.
 */
