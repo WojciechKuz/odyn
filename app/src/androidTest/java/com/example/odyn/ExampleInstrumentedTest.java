c271a205183edf74f19c8bf2e64035412b5059ae
Mateusz Szymczak <mszymczak710@o2.pl>
2023-05-21 12:15:28 +0200

    add authors and info about license

--- app/src/androidTest/java/com/example/odyn/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/odyn/ExampleInstrumentedTest.java
@@ -1,3 +1,10 @@
+/*
+    BSD 3-Clause License
+    Copyright (c) Wojciech Kuźbiński <wojkuzb@mat.umk.pl>, 2023
+
+    See https://aleks-2.mat.umk.pl/pz2022/zesp10/#/project-info for see license text.
+*/
+
 package com.example.odyn;
 
 import android.content.Context;
@@ -13,7 +20,7 @@ import static org.junit.Assert.*;
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @see <a href="http://d.android.com/tools/testing">Dokumentacja testowa</a>
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
